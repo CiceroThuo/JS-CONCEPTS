c007130be5f232cfa5553c10b2f2dea4a21a7546
CiceroThuo <thuocicero@gmail.com>
2024-01-29 11:00:08 +0000

    https://github.com/CiceroThuo/JS-CONCEPTS/blob/main/day2.js

--- day2.js
+++ day2.js
@@ -55,14 +55,80 @@
 //   if (i % 5 === 0) {
 //     console.log(number % 5);
 //   }
+// // }
+// let n = 15;
+// let sum = 0;
+// for (let i = 1; i < n; i++) {
+//   let rem = i % 3;
+//   let rem2 = i % 5;
+//   if (rem === 0 || rem2 === 0) {
+//     sum = sum + i;
+//   }
 // }
-let n = 15;
-let sum = 0;
-for (let i = 1; i < n; i++) {
-  let rem = i % 3;
-  let rem2 = i % 5;
-  if (rem === 0 || rem2 === 0) {
-    sum = sum + i;
-  }
-}
-console.log("sum:", sum);
+// console.log("sum:", sum);
+// function add(){ //function declaration (parameters)
+//  let a = 5;
+//   let b = 6;
+//   retrun a+b;
+// }
+// console.log(add())//function call
+
+// let num1 = 5;
+// let num2 = 6;
+
+// function add(a,b){ //function declaration (parameters)
+// //function definition
+
+//   return a + b
+//   //}
+//   //console.log(add(num1,num2))//function call
+
+//   //function expression
+//   const add = function(a,b){ 
+//     return a + b
+//   }
+//   console.log(10,20))
+//   //Arrow functions
+//   const add = (a,b) =>{
+//     return a + b
+//   }
+//   console.log(add2(10,20))
+
+// console.log('Cicero')
+// setTimeout(() =>{
+//   console.log('Hello')
+// }, 5000);
+// console.log('Shtickmann')
+
+// for(let i = 0; i <= 10; i++){
+//   console.log(i)
+// }
+// const name = 'Cicero'
+// const School = 'Zindua'
+
+// const sentence =`my name  is ${name} currently at ${School}`
+// console.log(sentence)
+
+
+// const factorial = (n) =>{
+//   if (n <=2 ){
+//     return n
+//   }
+//   return n * factorial(n-1)
+// }
+// console.log (factorial(6))
+//6! = 6*5*4*3*2*1
+
+
+// const fibonacci = (n) =>{
+//   //base case
+//   if(n === 1|| n === 2){
+//     return 1
+//   }
+//   if (n === 0){
+//     return 0
+//   }
+//   return fibonacci(n-1) + fibonacci(n-2)
+// }
+// console.log(fibonacci(5))
+// //0,1,1,2,3,5,
--- day4.js
+++ day4.js
@@ -1,29 +1,90 @@
-// //Groupby
-// // let arr =  [
-{name:"James",age : 25,city: "Nairobi"};
-{name:"Jack",age : 30,city: "New york"};
-{name:"Peter",age : 15,city: "Chicago"};
-{name:"John",age : 36,city: "Wisconsin"};
-];
-//  console.log[arr.groupBy("city"));
-// console.log(arr.groupBy("age"));
-// console.log(arr.groupBy("name"));
+// // //Groupby
+// // // let arr =  [
+// {name:"James",age : 25,city: "Nairobi"};
+// {name:"Jack",age : 30,city: "New york"};
+// {name:"Peter",age : 15,city: "Chicago"};
+// {name:"John",age : 36,city: "Wisconsin"};
+// ];
+// //  console.log[arr.groupBy("city"));
+// // console.log(arr.groupBy("age"));
+// // console.log(arr.groupBy("name"));
 
-{name:"James",age : 25,city: "Nairobi"};
-{name:"Jack",age : 30,city: "New york"};
-{name:"Peter",age : 15,city: "Chicago"};
-{name:"John",age : 36,city: "Wisconsin"};
-];
+// {name:"James",age : 25,city: "Nairobi"};
+// {name:"Jack",age : 30,city: "New york"};
+// {name:"Peter",age : 15,city: "Chicago"};
+// {name:"John",age : 36,city: "Wisconsin"};
+// ];
 
-function groupBy(array, property){
-let groupedItems = {}
-for(const item of array){
-  let val = item[property]
-  if(groupedItems[value]){
-    groupedItems[value] = {}
+// function groupBy(array, property){
+// let groupedItems = {}
+// for(const item of array){
+//   let val = item[property]
+//   if(groupedItems[value]){
+//     groupedItems[value] = {}
+//   }
+//   groupedItems[value].push(item)
+// }
+// return groupedItems
+// }
+// console.log(groupBy(a , "city"))
+
+
+/** 
+* @OBJECTS / DICTIONATIES
+
+*/
+// let person = {
+//   name : "James",	
+//   age : 25,
+ 
+//   school : " Zindua School",
+//   personalities : ["funny","smart","kind"],
+//   stubborn : false,
+//   hobbies : {
+//    outdoor : ["football","basketball","swimming"],
+//     indoor : ["sleeping","eating","watching movies"]
+//   },
+//   sound : function(){
+//   return "I am a human being"	
+//   }
+// }
+// console.table(Obejct.keys(person))
+// console.table(Obejct.values(person))
+
+let person =[
+  { 
+    name:"Alice",
+    age : "45",
+    city : "Nairobi"
+  },
+  
+  {
+     name : "Jack",
+     age  : "30",
+     city : "New york"
+  },
+    
+  {
+     name : "Peter",
+     age  :  "15",
+     city : "Chicago"
+    
+  },
+  {
+    name : "Dan",
+    age : "15",
+    city : "Chicago"
+  },
+
+]
+function groupBy(arr,property){
+  let grouped = {}
+  for(let i = 0; i < people.length; i++){
+    let person = arr[i]
+    let key =person('city')
+    console.log(key)
   }
-  groupedItems[value].push(item)
+ 
 }
-return groupedItems
-}
-console.log(groupBy(a , "city"))
+const person = 'Dan'
+groupBy(people, "city")
